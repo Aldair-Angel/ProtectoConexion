f116cc16bdba13aa73b55da43540b027bfd56374
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-11-30 13:19:18 -0500

    conexion al formulario

--- server.js
+++ server.js
@@ -1,22 +1,64 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+const app = express();  
+const cors = require("cors");
 
-const app = express();
+//archivos express json 
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+let conexion = mysql.createConnection({
+    host : 'localhost',
+    database : 'dbGerardoSalomonMejiaSaco',
+    user : 'root',
+    password : 'admin',
+});
+
+conexion.connect((err) => {
+    if(!err) {
+        console.log("conexion exitosa")
+    } else {
+        console.log("no hay conexion")
+    }
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/assets/index.html')   
 }) 
 
-// app.get('/style.css', (req, res) => {
-//     res.sendFile(__dirname + 'assets/css/index.css')   
-// }) 
-
-// app.use("/css",express.static(__dirname + 'assets/css/index.css'))
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/assets/index.html')   
+}) 
 
 app.use("/css", express.static(__dirname + '/assets/css'));
 app.use("/img", express.static(__dirname + '/assets/img'));
 app.use("/docs", express.static(__dirname + '/assets/docs'));
 app.use("/html", express.static(__dirname + '/assets/html'));
 app.use("/js", express.static(__dirname + '/assets/js'));
+
+app.post("/validar", function (req, res) {
+    const datos = req.body;
+    let nom = datos.nombre;
+    let apell = datos.apellido;
+    let cel = datos.celular;
+    let email = datos.correo;
+    let com = datos.mensaje;
+    //insertamos los datos a la tabla registro de la base de datos
+    const registrar = "INSERT INTO formulario (nombre,apellidos,gmail,celular,mensage) VALUES('" + nom + "','" + apell + "','" + email + "','" + cel + "','" + com + "')";
+  
+  //condicional al enviar los datos del formulario
+    conexion.query(registrar, function (error) {
+      if (error) {
+        throw error;
+      } else {
+        console.log("Datos almacenados correctamente")
+      }
+    })
+});
+
+    
 
 
 app.listen(3000);
