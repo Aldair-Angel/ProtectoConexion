cd478d668e0ac3d42cd2ed2c723dbef9ade1d16e
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-11-30 01:30:07 -0500

    conexion con html

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('helo world on express'))
+app.get('/', (req, res) => {
+    res.sendFile('index.html')   
+}) 
+
 
 app.listen(3000);
 console.log('Server on port 3000')
