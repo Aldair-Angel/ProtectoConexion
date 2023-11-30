025498ea40c69c3546f7073e907dd0e66ec71bba
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-11-30 10:58:40 -0500

    conexion completa

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')   
+    res.sendFile(__dirname + '/assets/index.html')   
 }) 
+
+// app.get('/style.css', (req, res) => {
+//     res.sendFile(__dirname + 'assets/css/index.css')   
+// }) 
+
+// app.use("/css",express.static(__dirname + 'assets/css/index.css'))
+
+app.use("/css", express.static(__dirname + '/assets/css'));
+app.use("/img", express.static(__dirname + '/assets/img'));
+app.use("/docs", express.static(__dirname + '/assets/docs'));
+app.use("/html", express.static(__dirname + '/assets/html'));
+app.use("/js", express.static(__dirname + '/assets/js'));
 
 
 app.listen(3000);
