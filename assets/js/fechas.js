52f5fe894d4377f100f39dec25657a4202fcfca8
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-12-05 11:52:48 -0500

    arreglo del calendario

--- assets/js/fechas.js
+++ assets/js/fechas.js
@@ -6,14 +6,14 @@ var fecha = fetch('http://localhost:3000/api/dates/'+ fechaActual)
     .then(res => res.json())
     .then((data)=> {
       if (data != null) {
-        document.getElementById('showDate').innerHTML = `<div class="calendario_conexion" role="alert" id="Datos_Tiempo">
+        document.getElementById('Datos_Tiempo').innerHTML = `<div class="calendario_conexion" role="alert" id="Datos_Tiempo">
         <h3>Hoy es ${fechaBn}</h3>
         <p>${data.fechas}</p>
         <p>${data.celebracion}</p>
         <p>${data.descripcion}</p>
       </div>`
       }else{
-        document.getElementById('showDate').innerHTML = `<div class="calendario_conexion" role="alert" id="Datos_Tiempo">
+        document.getElementById('Datos_Tiempo').innerHTML = `<div class="calendario_conexion" role="alert" id="Datos_Tiempo">
         <h3>Hoy no se celebra nada </h3>
       </div>` 
       }
