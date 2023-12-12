adc6c9605dcae3cbc87d94b0ad1de91b9bfa3fd5
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-12-12 16:49:13 -0500

    arreglo basedatos

--- assets/js/fechas.js
+++ assets/js/fechas.js
@@ -1,6 +1,6 @@
 const date = new Date;
-var fechaActual = date.getFullYear() + '-' + (date.getMonth()+1)+ '-'+ date.getDate();
-var fechaBn = date.getFullYear() + '/' + (date.getMonth()+1)+ '/'+ date.getDate();
+var fechaActual = date.getDate() + '-' + (date.getMonth()+1)+ '-'+ date.getFullYear();
+var fechaBn = date.getDate() + '/' + (date.getMonth()+1)+ '/'+ date.getFullYear();
 
 var fecha = fetch('http://localhost:3000/api/dates/'+ fechaActual)
     .then(res => res.json())
@@ -8,7 +8,6 @@ var fecha = fetch('http://localhost:3000/api/dates/'+ fechaActual)
       if (data != null) {
         document.getElementById('Datos_Tiempo').innerHTML = `<div class="calendario_conexion" role="alert" id="Datos_Tiempo">
         <h3>Hoy es ${fechaBn}</h3>
-        <p>${data.fechas}</p>
         <p>${data.celebracion}</p>
         <p>${data.descripcion}</p>
       </div>`
